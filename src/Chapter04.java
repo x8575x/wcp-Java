8cc13212732de9b6c8857a480329e74467951447
x8575x <101446609+x8575x@users.noreply.githug.com>
2022-10-28 21:20:34 +0900

    4章

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "kanae";
+		System.out.println(name);
+		
+		name = "BATMAN";
+		System.out.println(name);
+		
+		int Luckynum = 7;
+		System.out.println(Luckynum);
+	}
+
+}
---
+++ src/Voriable02.java
@@ -0,0 +1,7 @@
+
+public class Voriable02 {
+	public static void main(String[] args) {
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+	}
+}
